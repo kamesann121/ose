38feb8af948e4366e4ffdf2dc7615b710a4bb014
kamesann121 <futomitianzhong@gmail.com>
2026-02-08 12:14:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,20 +8,16 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
-// 静的ファイルの提供
 app.use(express.static('public'));
 
-// 待機中のプレイヤーとアクティブな試合を管理
 let waitingPlayer = null;
 let activeMatches = new Map();
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // マッチメイキング
   socket.on('findMatch', () => {
     if (waitingPlayer && waitingPlayer !== socket.id) {
-      // マッチング成立
       const matchId = `match_${Date.now()}`;
       const player1 = waitingPlayer;
       const player2 = socket.id;
@@ -34,9 +29,7 @@ io.on('connection', (socket) => {
         currentTurn: player1,
         stones: [],
         startTime: Date.now(),
-        duration: 180000,
-        turnStartTime: Date.now(),
-        turnDuration: 10000
+        turnStartTime: Date.now()
       };
 
       activeMatches.set(matchId, gameState);
@@ -56,13 +49,14 @@ io.on('connection', (socket) => {
       });
 
       waitingPlayer = null;
+      console.log('Match created:', matchId);
     } else {
       waitingPlayer = socket.id;
       socket.emit('waiting');
+      console.log('Player waiting:', socket.id);
     }
   });
 
-  // 石を配置
   socket.on('placeStone', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match && match.currentTurn === socket.id) {
@@ -70,17 +64,15 @@ io.on('connection', (socket) => {
       match.currentTurn = match.currentTurn === match.player1 ? match.player2 : match.player1;
       match.turnStartTime = Date.now();
 
-      // 相手に石配置を通知
       const opponentId = socket.id === match.player1 ? match.player2 : match.player1;
       io.to(opponentId).emit('opponentPlaced', { stone: data.stone });
-
-      // 両プレイヤーにゲーム状態更新
       io.to(match.player1).emit('gameUpdate', match);
       io.to(match.player2).emit('gameUpdate', match);
+      
+      console.log('Stone placed:', data.stone);
     }
   });
 
-  // 物理状態を更新
   socket.on('updatePhysics', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match) {
@@ -89,7 +81,6 @@ io.on('connection', (socket) => {
     }
   });
 
-  // 石の色変更
   socket.on('colorChange', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match) {
@@ -98,7 +89,6 @@ io.on('connection', (socket) => {
     }
   });
 
-  // 降参
   socket.on('surrender', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match) {
@@ -106,22 +96,20 @@ io.on('connection', (socket) => {
       io.to(match.player1).emit('gameOver', { winner, reason: 'surrender' });
       io.to(match.player2).emit('gameOver', { winner, reason: 'surrender' });
       activeMatches.delete(data.matchId);
+      console.log('Player surrendered');
     }
   });
 
-  // タイムアウト処理
   socket.on('turnTimeout', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match && match.currentTurn === socket.id) {
       match.currentTurn = match.currentTurn === match.player1 ? match.player2 : match.player1;
       match.turnStartTime = Date.now();
-
       io.to(match.player1).emit('gameUpdate', match);
       io.to(match.player2).emit('gameUpdate', match);
     }
   });
 
-  // 試合終了
   socket.on('gameEnd', (data) => {
     const match = activeMatches.get(data.matchId);
     if (match) {
@@ -131,7 +119,6 @@ io.on('connection', (socket) => {
     }
   });
 
-  // 切断処理
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
     
